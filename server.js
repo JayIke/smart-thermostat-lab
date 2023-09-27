2d96444a34ecf0e0de1542385c2899fbe646f09d
JayIke <jordineicher1@yahoo.com>
2023-09-26 19:46:20 -0500

    server receives data from Arduino.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
+
 const express = require('express');
 const app = express();
 
@@ -13,10 +15,47 @@ const { SerialPort, ReadlineParser } = require('serialport');
 
 require('dotenv').config();
 
-const client = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
+const arduinoAddress = '00:21:11:01:9A:A2'; // Replace with your Arduino's Bluetooth address
+
+btSerial.findSerialPortChannel(arduinoAddress, (channel) => {
+  btSerial.connect(arduinoAddress, channel, () => {
+    console.log('Connected to Arduino via Bluetooth');
+
+    // Send data to Arduinoc
+    btSerial.write(Buffer.from('Hello, Arduino!', 'utf-8'), (err, bytesWritten) => {
+      if (err) console.error(err);
+      console.log(`Sent ${bytesWritten} bytes to Arduino`);
+    });
+  }, () => {
+    console.error('Connection failed');
+  });
+});
+
+btSerial.on('data', (buffer) => {
+  // Handle data received from Arduino if needed
+  const data = buffer.toString('utf-8');
+  console.log(`Received data from Arduino: ${data}`);
+});
+
+// Handle server errors
+btSerial.on('error', (err) => {
+  console.error('Bluetooth serial port error:', err);
+});
+
+// Close the connection when the server exits
+process.on('SIGINT', () => {
+  console.log('Server exiting. Closing Bluetooth connection.');
+  btSerial.close(() => {
+    process.exit();
+  });
+});
+
+
+
+// const client = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
 
 // Configure the serial port
-const port = new SerialPort({port: '/dev/cu.usbmodem142301', baudRate: 9600, path: '/dev/cu.usbmodem142301', autoOpen: true}); // Replace 'COMX' with your Arduino's serial port
+/*const port = new SerialPort({port: '/dev/COM5', baudRate: 9600, path: '/dev/COM5', autoOpen: true}); // Replace 'COMX' with your Arduino's serial port
 
 // Create a parser to read lines from the Arduino
 const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
@@ -52,6 +91,7 @@ io.on('connection', (socket) => {
   })
 })
 
+
 port.on('open', () => {
   console.log('A device connected');
 });
@@ -67,23 +107,24 @@ parser.on("readable", function () {
   io.emit("graph new data", { time: Date.now(), value: data });
   if(Date.now() - lastSentMessage > 60000){
     if (data > info.maxtemp) {
-      client.messages.create({
-        body: "Your temperature has reached above "+info.maxtemp+" degrees Celcius.",
-        from: "+18443391255",
-        to: info.number,
-      });
+      // client.messages.create({
+      //   body: "Your temperature has reached above "+info.maxtemp+" degrees Celcius.",
+      //   from: "+18443391255",
+      //   to: info.number,
+      // });
       console.log("Your temperature has reached above "+info.maxtemp+" degrees Celcius.");
       lastSentMessage = Date.now();
     }
     else if(data < info.mintemp){
-      client.messages.create({
-        body: "Your temperature has declined below "+info.mintemp+" degrees Celcius.",
-        from: "+18443391255",
-        to: info.number,
-      });
+      // client.messages.create({
+      //   body: "Your temperature has declined below "+info.mintemp+" degrees Celcius.",
+      //   from: "+18443391255",
+      //   to: info.number,
+      // });
       console.log("Your temperature has declined below "+info.mintemp+" degrees Celcius.");
       lastSentMessage = Date.now();
     }
   }
   console.log("Data:", data);
 });
+*/
